f94ab9427bf7b274d026f88e666ad4ab2b4211fa
tathagatadasoft-hub <tathagata.dasoft@gmail.com>
2026-06-27 13:28:08 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,16 @@
+/*
+=========================================
+Supabase Configuration
+=========================================
+*/
 
+const SUPABASE_URL = "https://kxfvygnmcgvzsizwwlop.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_KPc7MdTlw9p79t1YGboASg_y94Ct98v";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+console.log("Supabase Connected");
